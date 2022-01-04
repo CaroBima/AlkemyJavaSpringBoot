9b8012068a761dd5c35316413e61e6d1e57afbe7
Caro Bima <carobima@gmail.com>
2022-01-04 19:18:10 -0300

    terminados los endpoint de character que permiten buscar por nombre y edad

--- src/main/java/com/Alkemy/Disney/controller/PersonajeController.java
+++ src/main/java/com/Alkemy/Disney/controller/PersonajeController.java
@@ -10,17 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/characters")
 public class PersonajeController {
     
     @Autowired
     private IPersonajeService personajeService;
             
           
-    @PostMapping("/new/personaje")//crea y modifica
+    @PostMapping("/new")//crea y modifica
     public void agregarPersonaje(@RequestBody Personaje personaje){
         personajeService.crearPersonaje(personaje);
     }
@@ -31,20 +34,26 @@ public class PersonajeController {
     }
     
     
-    @GetMapping("/characters")
+   /* @GetMapping("/characters")
     @ResponseBody  
     public List<Personaje> verPersonajes(){
         return personajeService.verPersonajes();
         
     }
+*/
     
-    @GetMapping(" /characters?age=edad")
+    @GetMapping(params="age")  
     @ResponseBody  
-    public List<Personaje> verPersonajesxedad(@PathVariable String edad){
-        return personajeService.buscarPersonajeXedad(edad);
+       public List<Personaje> verPersonajesxedad(@RequestParam("age") String age){
+        return personajeService.buscarPersonajeXedad(age);
         
     }
     
+    @GetMapping(params="name")  
+    public List<Personaje> verPersonajesxnombre(@RequestParam("name") String name){
+        return personajeService.buscarPersonajeXnombre(name);
+        
+    }
     
  
 }
